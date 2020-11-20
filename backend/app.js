a9da30d22ea3ac559dcc02ce8cfece418fe38f4a
0xb0b1 <vcente82@gmail.com>
2020-11-20 15:40:36 -0300

    ...

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const Quote = require('inspirational-quotes')
 
+
 const app = express()
+
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
 })
+
 
 app.get("/", function(req, res) {
     res.send(Quote.getQuote())
 })
 
+
 const port = process.env.PORT || 5000
+
 
 app.listen(port, function() {
     console.log("Server started successfully")
