fd9c20e6d800ed555c43208edd9716b70a07332e
0xb0b1 <vcente82@gmail.com>
2020-11-20 15:38:29 -0300

    ...

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,6 @@ const Quote = require('inspirational-quotes')
 
 const app = express()
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-})
-
 app.get("/", function(req, res) {
     res.send(Quote.getQuote())
 })
